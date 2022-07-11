0c4dde65464f7a1c4651e675b38694273864a9de
Ricardo Serrato <serrato.ricardo97@gmail.com>
2022-07-11 18:20:28 -0500

    Did powers of two problem from code wars and experimenting with github.

---
+++ kata/kyu8/powers_of_two.py
@@ -0,0 +1,14 @@
+# Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the
+# exponent ranging from 0 to n ( inclusive ).
+
+# Examples
+# n = 0  ==> [1]        # [2^0]
+# n = 1  ==> [1, 2]     # [2^0, 2^1]
+# n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+def powers_of_two(n):
+    new_list = []
+    for i in range(n+1):
+        new_list.append(2 ** i)
+    return new_list
+
